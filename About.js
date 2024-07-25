e985f869823dbae0aa951040a8b86d7a5b6b4abd
Priyanshu Pachhola <149190023+Priyanshu08-boop@users.noreply.github.com>
2024-07-25 19:04:10 +0530

    Add files via upload

---
+++ About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is about componenet
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptatum perspiciatis tempora consequatur praesentium dolores tenetur ea eveniet commodi, animi nihil quisquam suscipit sequi necessitatibus! Tenetur eligendi corporis labore illo.</p>
+        </div>
+    )
+}
---
+++ Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Footer = () => {
+  
+  return (
+    <footer className="bg-dark text-light my-4" >
+      <p className="text-center">
+        Copyright &copy;MyTodosList.com
+      </p>
+    </footer>
+  )
+}
